b609fe87996fca437d66042bfd857cfd5c31f41e
EchoApeiron <dion.pezzimenti@protonmail.com>
2020-09-11 23:51:20 -0400

    Changed layout, updated javascript, and added mild responsiveness

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let button = document.getElementById('login-button'); 
-
-    button.addEventListener('click', () => {
+    function onClick() {
         let username = document.getElementById('input__username').value;
         let password = document.getElementById('input__password').value; 
         alert(`Username: ${username}\nPassword: ${password}`);
         button.blur(); 
+        document.getElementById('input__username').value = '';
+        document.getElementById('input__password').value = ''; 
+    }
+
+    let button = document.getElementById('login-button'); 
+
+    button.addEventListener('click', () => {
+        onClick();
+    });
+
+    button.addEventListener('keyup', (e) => {
+        if (e.key == 'Enter') {
+            onClick();
+        }
     });
 });
